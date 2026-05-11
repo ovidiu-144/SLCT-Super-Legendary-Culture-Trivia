89abe0cdd31a87e2f01c1fd837c26273e1ed5484
Alesia Lefter <alesia.lefter@student.tuiasi.ro>
2026-05-11 23:46:54 +0300

    Comentarii SLCT

--- SLCT/MainMenu.cs
+++ SLCT/MainMenu.cs
@@ -1,4 +1,15 @@
-﻿using System;
+﻿// Proiect:      SLCT – Super Legendary Culture Trivia
+// Fișier:       MainMenu.cs
+// Autor:        Alesia Lefter
+// Echipă:       Alesia, Ioana, Ovidiu, Catalin
+// Materie:      Ingineria Programării
+// Descriere:    Formularul principal al aplicației. Este primul
+//               ecran văzut de utilizator și oferă opțiunile
+//               de a începe jocul sau de a ieși din aplicație.
+
+
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,8 +21,18 @@ using System.Windows.Forms;
 
 namespace SLCT
 {
+    /// <summary>
+    /// Formularul principal (ecranul de start) al aplicației SLCT.
+    /// Se deschide maximizat și fără bordură, ocupând tot ecranul.
+    /// Oferă navigare către meniul de categorii sau închiderea aplicației.
+    /// </summary>
     public partial class MainMenu : Form
     {
+        /// <summary>
+        /// Inițializează formularul principal.
+        /// Setează fereastra ca maximizată, fără bordură și cu
+        /// poziție manuală pentru o experiență fullscreen.
+        /// </summary>
         public MainMenu()
         {
             InitializeComponent();
@@ -21,11 +42,25 @@ namespace SLCT
             this.StartPosition = FormStartPosition.Manual; 
         }
 
+        /// <summary>
+        /// Handler pentru butonul „Exit".
+        /// Închide complet aplicația.
+        /// </summary>
+        /// <param name="sender">Butonul care a declanșat evenimentul.</param>
+        /// <param name="e">Datele evenimentului de click.</param>
         private void buttonExit_Click(object sender, EventArgs e)
         {
             Application.Exit();
         }
 
+        /// <summary>
+        /// Handler pentru butonul „Start Game".
+        /// Deschide meniul de selectare a categoriei, transferând
+        /// poziția, dimensiunea și starea ferestrei curente,
+        /// apoi ascunde formularul principal.
+        /// </summary>
+        /// <param name="sender">Butonul care a declanșat evenimentul.</param>
+        /// <param name="e">Datele evenimentului de click.</param>
         private void buttonStart_Click(object sender, EventArgs e)
         {
             CategoryMenu categoryMenu = new CategoryMenu();
